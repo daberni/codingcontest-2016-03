54a8ba17f584d2d9b9ba0a1e680d9e4a96c31ce2
Bernhard Danecker <bernhard.danecker@gmail.com>
2017-03-31 14:40:39 +0200

    level2

--- src/CCC17Main.kt
+++ src/CCC17Main.kt
@@ -1,35 +1,42 @@
 import java.io.File
 
-data class Location(val name: String, val x: Int, val y: Int)
 
-class Trip(val locations: List<Location>)
-{
-    fun getTravelTime() {
-
-
-
-
-    }
-}
+open class Location(val name: String, val x: Int, val y: Int)
 
 fun main(args: Array<String>) {
-    val lines = File("input/level1-4.txt").readLines()
+    val lines = File("input/level2-4.txt").readLines()
 
     val numberOfLocations = lines[0].toInt()
     val locations = lines.subList(1, numberOfLocations + 1).map {
         val parts = it.split(" ")
         Location(parts[0], parts[1].toInt(), parts[2].toInt())
     }
-    val line = lines.last().split(" ")
+
+    val journeyTrip = lines[lines.size - 2].split(" ")
+
+    val tripStart = locations.single { it.name == journeyTrip.first() }
+    val tripEnd = locations.single { it.name == journeyTrip.last() }
+
+    val hyperloopTrip = lines[lines.size - 1].split(" ")
 
 
-    val start = locations.single { it.name == line.first() }
-    val end = locations.single { it.name == line.last() }
+    val hyperloopLocations = locations.filter { hyperloopTrip.contains(it.name) }
+    val startHyperloop = hyperloopLocations.sortedBy { pythagoras(tripStart, it) }.first()
+    val destinationHyperloop = hyperloopLocations.single { it.name != startHyperloop.name}
 
-    val distance = pythagoras(start, end)
-    val time = Math.round(distance / 250 + 200).toInt()
+    val hyperloopDistance = pythagoras(startHyperloop, destinationHyperloop)
+    val hyperloopTime = hyperloopDistance / 250 + 200
 
-    print(time)
+    val driveDistance = pythagoras(tripStart, startHyperloop)
+    val driveTime = driveDistance / 15
+
+    val walkDistance = pythagoras(destinationHyperloop, tripEnd)
+    val walkTime = walkDistance / 15
+
+    val totalTime = listOf(driveTime, hyperloopTime, walkTime).sum()
+
+    println(totalTime)
+    println(Math.round(totalTime))
 }
 
 
