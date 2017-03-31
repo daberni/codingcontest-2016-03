1573f8ca36bdc2057082b57bbd41903fb06de461
Lukas Fink <lukas.fink@troii.com>
2017-03-31 14:19:46 +0200

    level 1

--- src/CCC17Main.java
+++ src/CCC17Main.java
@@ -1,8 +1,27 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CCC17Main {
 
     public static void main(String[] args) {
-        for (String line : FileHelper.readAllLines("test.txt")) {
-            System.out.println(line);
+        List<String> lines = FileHelper.readAllLines("level1-4.txt");
+        int numberOfLocations = Integer.parseInt(lines.get(0));
+        Map<String, Location> locations = new HashMap<>(numberOfLocations);
+        int i;
+        for (i = 1; i < 1 + numberOfLocations; i++) {
+            String[] lineSplits = lines.get(i).split(" ");
+            locations.put(lineSplits[0], new Location(lineSplits[0], Integer.parseInt(lineSplits[1]),
+                    Integer.parseInt(lineSplits[2])));
         }
+
+        Location location1 = locations.get(lines.get(i).split(" ")[0]);
+        Location location2 = locations.get(lines.get(i).split(" ")[1]);
+        double distance = getDistance(location1, location2);
+        System.out.println(Math.round(distance / 250 + 200));
+    }
+
+    private static double getDistance(Location location1, Location location2) {
+        return Math.sqrt(Math.pow(location1.x - location2.x, 2) + Math.pow(location1.y - location2.y, 2));
     }
 }
---
+++ src/Location.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lukas on 31/03/17.
+ */
+public class Location {
+
+    public String name;
+    public int x;
+    public int y;
+
+    public Location(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+}
